4d6abca23edae632aa534063b66df58fb2297111
Adam PM <adam.pm77@gmail.com>
2025-08-22 00:50:46 +0700

    Update footer and team language files for consistency in business partner references

--- lang/id/footer.php
+++ lang/id/footer.php
@@ -2,6 +2,6 @@
 
 return [
     'description' => 'VGI berkomitmen untuk mempromosikan gaya hidup sehat melalui solusi inovatif di bidang suplemen kesehatan untuk makanan dan minuman.',
-    'info' => 'VGI merupakan mitra bisnis BP Group milik Ippho Santosa.',
+    'info' => 'VGI merupakan Mitra Bisnis BP Group milik Ippho Santosa.',
     'product' => 'Produk',
 ];
